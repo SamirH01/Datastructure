fe5dda4eb4fd465713c4c74fc69024f096587278
Samir <samirhu@student.chalmers.se>
2023-10-31 19:50:06 +0100

    test

--- Test.py
+++ Test.py
@@ -7,3 +7,18 @@ def sequential_search(elements, e):
 list = [1, 2, 5, 6, 7, 8]
 
 print(sequential_search(list, 8))
+
+def binary_search(elements, e):
+    low = 0
+    high = len(elements)-1
+    while(low <= high):
+        mid = (low+high)//2
+        if (elements[mid] > e):
+            high = mid-1
+        elif (elements[mid] < e):
+            low = mid+1
+        else: 
+            return mid
+    return -1
+
+print(binary_search(list, 8))
